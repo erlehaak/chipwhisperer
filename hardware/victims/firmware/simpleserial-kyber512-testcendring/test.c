c1c6418ec8465620ab9af636228af06a79959e29
erlen <erlend_haakegaard@hotmail.com>
2023-03-21 19:48:43 +0100

    added get_pk to print whole public key

--- hardware/victims/firmware/simpleserial-kyber512-testcendring/test.c
+++ hardware/victims/firmware/simpleserial-kyber512-testcendring/test.c
@@ -5,30 +5,39 @@
 
 #include <string.h>
 
+unsigned char sk[KYBER_SECRETKEYBYTES];
+unsigned char pk[KYBER_PUBLICKEYBYTES];
+unsigned char ss_a[KYBER_SSBYTES], ss_b[KYBER_SSBYTES];
+unsigned char send[KYBER_CIPHERTEXTBYTES];
+
 static int test_keys(void)
 {
-  unsigned char sk[KYBER_SECRETKEYBYTES];
-  unsigned char pk[KYBER_PUBLICKEYBYTES];
-  unsigned char ss_a[KYBER_SSBYTES], ss_b[KYBER_SSBYTES];
-  unsigned char send[KYBER_CIPHERTEXTBYTES];
+  
   
   simpleserial_put('r', 32, pk);
   //Alice generates a public key
   PQCLEAN_KYBER512_CLEAN_crypto_kem_keypair(pk, sk);
 
   //Bob derives a secret key and creates a response
-  PQCLEAN_KYBER512_CLEAN_crypto_kem_enc(send, ss_b, pk);
+  //PQCLEAN_KYBER512_CLEAN_crypto_kem_enc(send, ss_b, pk);
 
   //Alice uses Bobs response to get her secret key
-  PQCLEAN_KYBER512_CLEAN_crypto_kem_dec(ss_a, send, sk);
+  //PQCLEAN_KYBER512_CLEAN_crypto_kem_dec(ss_a, send, sk);
 
   simpleserial_put('f', 16, pk);
   return 0;
 }
 
-static uint8_t get_pk(void)
+uint8_t get_pk(void)
 {
-  return pk;
+  int i;
+
+  for (i = 0; i < KYBER_PUBLICKEYBYTES; i += 32) {
+        char chunk[33]; // 32 chars plus a null terminator
+        strncpy(chunk, pk + i, 32); // copy 32 chars into the chunk array
+        chunk[32] = '\0'; // add a null terminator to the end of the chunk
+        simpleserial_put('r', 32, chunk);
+  }
 }
 
 
